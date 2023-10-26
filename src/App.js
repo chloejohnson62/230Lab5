d938b8660cfdb4ce38c43c63b67d80cabdfe8647
chloejohnson62 <cmj00004@mix.wvu.edu>
2023-10-25 20:15:47 -0400

    update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>"CS 230L</h1>
-      <h2>"Section - 03"</h2>
-      <p>"WVU ID: 800357587"</p>
-      <p>"Hi I am Chloe Johnson</p>
+      <h1>CS 230L</h1>
+      <h2>Section - 03</h2>
+      <p>WVU ID: 800357587</p>
+      <p>Hi I am Chloe Johnson</p>
     </div>
   );
 }
