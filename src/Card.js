b99baffbd10cad74ddc81c1eeb6960fc9e02ec32
chloejohnson62 <122559465+chloejohnson62@users.noreply.github.com>
2023-10-31 14:08:47 -0400

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ function Card() {
     <div className="card" style={{ width: '18rem' }}>
       <img src="your-image-source" className="card-img-top" alt="Card Image" />
       <div className="card-body">
-        <h5 className="card-title">Card Title</h5>
+        <h5 className="card-title">Card 01</h5>
         <p className="card-text">Some quick example text to build on the card.</p>
         <a href="#" className="btn btn-primary">Go somewhere</a>
       </div>
