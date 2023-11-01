fded694cc59e46f9830db7d90400ded177857e13
chloejohnson62 <122559465+chloejohnson62@users.noreply.github.com>
2023-11-01 16:11:15 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
 import logo from './logo.svg';
 import Navbar from './Navbar';
 import Card from './Card';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-  
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-    <h1>CS 230L</h1>
+     <Navbar />
+     <h1>CS 230L</h1>
     <h2>Section 03</h2>
     <p>WVU ID 800357587</p>
     <p> Hi! I am Chloe</p>
-    <Card />
+     <Card />
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
 function Navbar() {
+
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Your App Name</a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <a className="navbar-brand text-white" href="#">NavBar</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-link text-white" href="#">Home <span className="sr-only"></span></a>
           </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">About</a>
+                    <a className="nav-link text-white" href="#">About</a>
           </li> 
                 <li className="nav-item">
-                     <a className="nav-link" href="#">Contact</a>
+                     <a className="nav-link text-white" href="#">Contact</a>
                 </li>
              </ul>
           </div>
