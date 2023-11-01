6ea5f3074b964104bc037750b10c13f203692486
chloejohnson62 <122559465+chloejohnson62@users.noreply.github.com>
2023-11-01 14:53:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import logo from './logo.svg';
 import Navbar from './Navbar';
 import Card from './Card';
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.css";
+  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+      <Navbar />
+    <Card />
     </div>
   );
 }
