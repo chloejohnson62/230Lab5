aea26df4be60f56ff350f1ca7e424aeb34af3be5
chloejohnson62 <122559465+chloejohnson62@users.noreply.github.com>
2023-11-01 14:49:10 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -7,10 +7,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+     <Navbar />
+     <Card />
     </div>
   );
 }
